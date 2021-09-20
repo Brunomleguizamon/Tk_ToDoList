3297b63248dee51a5f4bd56ce3f36c71cff28ac7
Bmleguizamon <brunomleguizamon@gmail.com>
2021-09-20 22:24:04 +0200

    update database

--- db.py
+++ db.py
@@ -5,8 +5,8 @@ root = Tk()
 root.title('To Do List')
 root.geometry('500x500')
 
-coon = sqlite3.connect('todo.db')
-c = coon.cursor()
+conn = sqlite3.connect('todo.db')
+c = conn.cursor()
 
 c.execute("""
     CREATE TABLE if not exists todo(
@@ -19,3 +19,40 @@ c.execute("""
 
 conn.commit()
 
+def renderTodo():
+    rows = c.execute("SELECT * FROM todo").fetchall()
+
+    for i in range(0, len(rows)):
+        completed = rows[i][3]
+        description = rows[i][2]
+        l = Checkbutton(frame, text= description, width=42, anchor="w")
+        l.grid(row=i, column=0, sticky="w")
+
+def addTodo():
+    todo = e.get()
+    if todo:
+        c.execute("""
+            INSERT INTO todo (description, completed) VALUES(?, ?)
+                """, (todo, False))
+        conn.commit()
+        e.delete(0, END)
+        renderTodo()
+    
+
+l = Label(root, text="Task")
+l.grid(row=0, column=0)
+
+e = Entry(root, width=40)
+e.grid(row=0, column=1)
+
+btn = Button(root, text="Add", command= addTodo)
+btn.grid(row=0, column=2)
+
+frame = LabelFrame(root, text="My Task", padx=5, pady=5)
+frame.grid(row=1,column=0, columnspan=3,sticky="nswe", padx=5)
+
+e.focus()
+root.bind('<Return>', lambda x: addTodo())
+
+
+root.mainloop()
