3a6d021403b23119afa4490bfba76ad2e2d92575
Bmleguizamon <brunomleguizamon@gmail.com>
2021-09-21 11:03:41 +0200

    update delete task

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Django_ToDoList
 
+To do list created in python using tkinter and sqlite3
+
+To run it you should only have python installed, since these are installed by default
+
+
 
--- db.py
+++ db.py
@@ -16,17 +16,39 @@ c.execute("""
         completed BOOLEAN NOT NULL
     );
 """)
-
 conn.commit()
+
+# Curryng!!
+def complete(id):
+    def _complete():
+        todo = c.execute("SELECT * from todo WHERE id = ?", (id, )).fetchone()
+        c.execute("UPDATE todo SET completed = ? WHERE id = ?",(not todo[3], id))
+        conn.commit()
+        renderTodo()
+    return _complete
+
+def remove(id):
+    def _remove():
+        c.execute("DELETE FROM todo WHERE id = ?",(id, ))
+        conn.commit()
+        renderTodo()
+    return _remove
 
 def renderTodo():
     rows = c.execute("SELECT * FROM todo").fetchall()
+    for widget in frame.winfo_children():
+        widget.destroy()
 
     for i in range(0, len(rows)):
+        id = rows[i][0]
         completed = rows[i][3]
         description = rows[i][2]
-        l = Checkbutton(frame, text= description, width=42, anchor="w")
+        color = '#A0A0A0' if completed else '#000000'
+        l = Checkbutton(frame, text= description, width=42, fg=color , anchor="w", command=complete(id))
         l.grid(row=i, column=0, sticky="w")
+        l.select() if completed else l.deselect()
+        btn =  Button(frame, text="Delete", command=remove(id))
+        btn.grid(row=i, column=1)
 
 def addTodo():
     todo = e.get()
@@ -37,7 +59,6 @@ def addTodo():
         conn.commit()
         e.delete(0, END)
         renderTodo()
-    
 
 l = Label(root, text="Task")
 l.grid(row=0, column=0)
@@ -54,5 +75,6 @@ frame.grid(row=1,column=0, columnspan=3,sticky="nswe", padx=5)
 e.focus()
 root.bind('<Return>', lambda x: addTodo())
 
+renderTodo()
 
 root.mainloop()
